5e365165d7b02ed296784148e59e8230f378ef63
yvan <ytouko@lekruu.com>
2025-11-22 16:13:48 +0100

    ajout du fichier index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'todolist');
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
